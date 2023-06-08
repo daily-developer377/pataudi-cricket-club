810b8c44b2aa85ec8786257cc0bff17d098997e8
Nandhu sunil <nandhusunil377@gmail.com>
2023-06-08 23:47:17 +0530

    uncomment the commentes portion oƒ bootstrap js link in bottom

--- backToTop.js
+++ backToTop.js
@@ -1,17 +1,17 @@
 // <!-- ====== SCROLL TO TOP SCRIPT ====== -->
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
-var rootElement = document.documentElement
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
 
 function handleScroll() {
   // Do something on scroll - 0.15 is the percentage the page has to scroll before the button appears
   // This can be changed - experiment
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.15) {
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.15) {
     // Show button
-    scrollToTopBtn.classList.add("showBtn")
+    scrollToTopBtn.classList.add("showBtn");
   } else {
     // Hide button
-    scrollToTopBtn.classList.remove("showBtn")
+    scrollToTopBtn.classList.remove("showBtn");
   }
 }
 
@@ -19,8 +19,8 @@ function scrollToTop() {
   // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
-    behavior: "smooth"
-  })
+    behavior: "smooth",
+  });
 }
-scrollToTopBtn.addEventListener("click", scrollToTop)
-document.addEventListener("scroll", handleScroll)
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
